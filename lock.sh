917b4f1f0ff712d6b003e4042566c6811d950f23
Jesse Evers <jesse27999@gmail.com>
2015-04-20 11:30:55 -0400

    got lock.sh working

--- lock.sh
+++ lock.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# scrot /tmp/screen.png
-sh ./induvidual_scrot.sh
-convert /tmp/head_0.png -scale 10% -scale 1000% /tmp/head_0.png
-[[ -f $HOME/pictures/misc/lock-image.png ]] && convert /tmp/head_0.png $HOME/pictures/misc/lock-image.png -gravity center -composite -matte /tmp/head_0.png
-if [ -a /tmp/head_1.png ]; then
-  convert /tmp/head_1.png -scale 10% -scale 1000% /tmp/head_1.png
-  [[ -f $HOME/pictures/misc/lock-image.png ]] && convert /tmp/head_1.png $HOME/pictures/misc/lock-image.png -gravity center -composite -matte /tmp/head_1.png
-  convert /tmp/head_0.png /tmp/head_1.png +append /tmp/screen.png
+#if [ -a $HOME/tmp/head_0.png ]; then
+#  rm $HOME/tmp/head_0.png
+#fi
+
+#if [ -a $HOME/tmp/head_1.png ]; then
+#  rm $HOME/tmp/head_1.png
+#fi
+
+sh $HOME/desktop/dotfiles/induvidual_scrot.sh
+convert $HOME/tmp/head_0.png -scale 10% -scale 1000% $HOME/tmp/head_0.png
+[[ -f $HOME/pictures/misc/lock-image.png ]] && convert $HOME/tmp/head_0.png $HOME/pictures/misc/lock-image.png -gravity center -composite -matte $HOME/tmp/head_0.png
+if [ -e $HOME/tmp/head_1.png ]; then
+  convert $HOME/tmp/head_1.png -scale 10% -scale 1000% $HOME/tmp/head_1.png
+  [[ -f $HOME/pictures/misc/lock-image.png ]] && convert $HOME/tmp/head_1.png $HOME/pictures/misc/lock-image.png -gravity center -composite -matte $HOME/tmp/head_1.png
+  convert $HOME/tmp/head_0.png $HOME/tmp/head_1.png +append $HOME/tmp/screen.png
 else
-  mv /tmp/head_0.png /tmp/screen.png
+  mv $HOME/tmp/head_0.png $HOME/tmp/screen.png
 fi
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -u -i /tmp/screen.png
+i3lock -u -i $HOME/tmp/screen.png
