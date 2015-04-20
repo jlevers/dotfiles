7f29bbf632addd112b3613551a90ddbac74ccc39
Jesse Evers <jesse27999@gmail.com>
2015-04-20 11:37:34 -0400

    variablized paths in lock.sh

--- lock.sh
+++ lock.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+TEMPDIR=$HOME/tmp
+
 sh $HOME/desktop/dotfiles/induvidual_scrot.sh
-convert $HOME/tmp/head_0.png -scale 10% -scale 1000% $HOME/tmp/head_0.png
-[[ -f $HOME/pictures/misc/lock-image.png ]] && convert $HOME/tmp/head_0.png $HOME/pictures/misc/lock-image.png -gravity center -composite -matte $HOME/tmp/head_0.png
-if [ -e $HOME/tmp/head_1.png ]; then
-  convert $HOME/tmp/head_1.png -scale 10% -scale 1000% $HOME/tmp/head_1.png
-  [[ -f $HOME/pictures/misc/lock-image.png ]] && convert $HOME/tmp/head_1.png $HOME/pictures/misc/lock-image.png -gravity center -composite -matte $HOME/tmp/head_1.png
-  convert $HOME/tmp/head_0.png $HOME/tmp/head_1.png +append $HOME/tmp/screen.png
+convert $TEMPDIR/head_0.png -scale 10% -scale 1000% $TEMPDIR/head_0.png
+[[ -f $HOME/pictures/misc/lock-image.png ]] && convert $TEMPDIR/head_0.png $HOME/pictures/misc/lock-image.png -gravity center -composite -matte $TEMPDIR/head_0.png
+if [ -e $TEMPDIR/head_1.png ]; then
+  convert $TEMPDIR/head_1.png -scale 10% -scale 1000% $TEMPDIR/head_1.png
+  [[ -f $HOME/pictures/misc/lock-image.png ]] && convert $TEMPDIR/head_1.png $HOME/pictures/misc/lock-image.png -gravity center -composite -matte $TEMPDIR/head_1.png
+  convert $TEMPDIR/head_0.png $TEMPDIR/head_1.png +append $TEMPDIR/screen.png
 else
-  mv $HOME/tmp/head_0.png $HOME/tmp/screen.png
+  mv $TEMPDIR/head_0.png $TEMPDIR/screen.png
 fi
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -u -i $HOME/tmp/screen.png
+i3lock -u -i $TEMPDIR/screen.png
